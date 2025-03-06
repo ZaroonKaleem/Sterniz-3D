443317e711643b056ed9d80deace73bb53d90401
Mian Zaroon <161944277+ZaroonKaleem@users.noreply.github.com>
2025-03-06 09:24:23 -0800

    Logo fixed

--- src/assets/index.js
+++ src/assets/index.js
@@ -7,7 +7,7 @@ import github from "./github.png";
 import menu from "./menu.svg";
 import close from "./close.svg";
 import code1 from "./code1.png";
-import sternizBg from "./sterniz-bg.png"
+import sternizBg from "./Sterniz-bg.png"
 import about from "./about-us.jpg"
 
 import css from "./tech/css.png";
